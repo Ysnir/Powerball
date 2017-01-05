14c1a89e54a59b7f84fef42cdca1ba336dd6bb43
Yann <nayplatypusmith@live.fr>
2017-01-05 14:36:30 +0100

    Working on score Storage

---
+++ assets/Scripts (3)/HighscoreScript (43)/script.ts
@@ -0,0 +1,15 @@
+class HighscoreScriptBehavior extends Sup.Behavior {
+  highscores = [];
+  
+  awake() {
+    if(Sup.Storage.getJSON("highscores")){
+      this.highscores = Sup.Storage.getJSON("highscores");
+    }
+    for(let i=0; i<5; i++) {
+      Sup.getActor("HallOfFame").getChildren()[i].getChild("Name").textRenderer.setText(this.highscores[i].name);
+      Sup.getActor("HallOfFame").getChildren()[i].getChild("Score").textRenderer.setText(this.highscores[i].score);
+    }
+  }
+
+}
+Sup.registerBehavior(HighscoreScriptBehavior);
